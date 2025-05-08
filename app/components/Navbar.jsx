a283d507eeb87a73c786c82c98027265335a386a
Valerio Franchi <vlrfranchi1@gmail.com>
2025-05-08 22:51:03 +0200

    prova

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+
+export default function Navbar() {
+  return (
+    <div>
+      <nav>
+          <h1>Dojo Helpdesk</h1>
+          <Link href="/">Dashboard</Link>
+          <Link href="/tickets">Tickets</Link>
+        </nav>
+    </div>
+  )
+}
