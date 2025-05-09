e358c434dbfabad5c99538df5124d4c4e437a318
Valerio Franchi <105786900+Siddha95@users.noreply.github.com>
2025-05-09 14:46:01 +0000

    logo

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,10 +1,17 @@
 import Link from 'next/link'
-
+import Image from 'next/image'
+import Logo from './Ardanese-logo.png'
 
 export default function Navbar() {
   return (
     <div>
       <nav>
+          <Image  src={Logo} 
+            alt='Logo'
+            width={200}
+            quality={100}
+            placeholder='blur'
+          />
           <h1>Dojo Helpdesk</h1>
           <Link href="/">Dashboard</Link>
           <Link href="/tickets">Tickets</Link>
