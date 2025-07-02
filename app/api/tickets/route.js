1b70f7398f4bfd0306ab62a88e1e95c9d1aac5d4
Valerio Franchi <105786900+Siddha95@users.noreply.github.com>
2025-07-02 13:09:39 +0000

    post

--- app/api/tickets/route.js
+++ app/api/tickets/route.js
@@ -1,6 +1,8 @@
 // This file defines an API route in a Next.js application that returns a list of tickets.
 import { NextResponse } from 'next/server';
+
 export const dynamic = 'force-dynamic';    // Ensures the route is always dynamic
+
 export async function GET() {
     const res = await fetch('http://localhost:4000/tickets'  )
     // Example: return a JSON response with tickets
@@ -8,5 +10,23 @@ export async function GET() {
 
     return NextResponse.json(tickets, {
         status: 200,       
+    })
+}
+
+
+export async function POST(request) {
+    const ticket = await request.json();    
+
+    const res = await fetch('http://localhost:4000/tickets', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(ticket),
+    })
+
+    const newTicket = await res.json();
+
+    return NextResponse.json(newTicket, {
+        status: 201,
     })
 }
