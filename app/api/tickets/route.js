2358001b39d713a9bf1477725554ffa8c06c9f67
Valerio Franchi <105786900+Siddha95@users.noreply.github.com>
2025-07-02 12:54:04 +0000

    route

---
+++ app/api/tickets/route.js
@@ -0,0 +1,11 @@
+// This file defines an API route in a Next.js application that returns a list of tickets.
+import { NextResponse } from 'next/server';
+export async function GET() {
+    const res = await fetch('http://localhost:4000/tickets')
+    // Example: return a JSON response with tickets
+    const tickets = await res.json()
+
+    return NextResponse.json(tickets, {
+        status: 200,       
+    })
+}
